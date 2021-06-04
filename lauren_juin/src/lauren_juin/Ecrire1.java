397ec68e6ec4ea59f77e2249b8bba12303a9ebe0
laurenkwong <laurenkwong@192.168.2.18>
2021-06-04 14:57:26 -0400

    Le 4 juin 2021

---
+++ lauren_juin/src/lauren_juin/Ecrire1.java
@@ -0,0 +1,36 @@
+/*
+* Author : laurenkwong
+* Date : Jun. 4, 2021
+* Description : 
+*/
+
+package lauren_juin;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Ecrire1 {
+
+	public static void main(String[] args) {
+		String Fichier="Ecrire.txt";
+		final String s1="Voici ce que je voudrais";
+		final String s2="écrire et vous faire comprendre";
+		
+		FileWriter ficAEcrire;
+		PrintWriter sortie;
+		try
+		{
+			ficAEcrire=new FileWriter(Fichier);
+			sortie=new PrintWriter(ficAEcrire);
+			sortie.print(s1);
+			sortie.print(s2);
+			sortie.close();
+		} catch(IOException ioe)
+		{
+			System.out.println("Erreur entrée/sortie");
+		}
+
+	}
+
+}
